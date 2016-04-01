e61c6a934d24c076ea1f212cb2c7a1da83f61157
tomatagolo <tom@agolo.com>
2016-03-31 22:53:30 -0400

    add bitonic array example

--- ch_1/interview_questions/06_bitonic_array.js
+++ ch_1/interview_questions/06_bitonic_array.js
@@ -9,3 +9,80 @@
  * can guarantee to perform fewer than ~2lgN compares in the worst case)
  */
 
+'use strict';
+
+class BinarySearch{
+  constructor(){ }
+  rank(key, arr){
+    // console.log('RANK', key, arr);
+    let lo = 0, hi = arr.length-1;
+    while (lo <= hi){
+      let mid = Math.floor(lo + (hi - lo)/2);
+      // console.log('NEW MID', lo, hi, mid);
+      if (key > arr[mid]){
+        lo = mid + 1;
+      } else if (key < arr[mid]){
+        hi = mid - 1;
+      } else {
+        return mid;
+      }
+    }
+    return -1;
+  }
+};
+
+class AlternateBinarySearch{
+  constructor(){ }
+  findMax(arr){
+    console.log('FIND MAX', arr);
+    let lo = 0, hi = arr.length-1;
+    while (arr[Math.floor(lo + (hi - lo)/2)] > arr[Math.floor(lo + (hi - lo)/2) - 1]){
+      let mid = Math.floor(lo + (hi - lo)/2);
+      console.log('MID', mid);
+      if (arr[mid] > arr[mid+1] && arr[mid] > arr[mid-1]){
+        return mid;
+      } else if (arr[mid] < arr[mid-1]){
+        hi = mid - 1; 
+      } else if (arr[mid] < arr[mid-1]){
+        lo = mid + 1;
+      }
+    }
+  }
+};
+
+let arr = [0, 2, 5, 7, 100, 80, 50, 20, 4, 1];
+
+class Bitonic{
+  constructor(arr){
+    this.a = arr;
+    /*
+    for(let i=1; i<arr.length; i++){ // N-1 worst case complexity
+      if (arr[i] < arr[i-1]){
+        this.mid = i - 1;
+        break;
+      }
+    }
+   */
+  let ab = new AlternateBinarySearch();
+  this.mid = ab.findMax(arr);
+  }
+  contains(value){
+    let b = new BinarySearch();
+    // both combined make log N worst case complexity
+    let search1 = b.rank(value, this.a.slice(0, this.mid)); 
+    let search2 = b.rank(value, this.a.slice(this.mid).reverse());
+    if (search1 == -1 && search2 == -1){
+      return false;
+    } else {
+      return true;
+    }
+  }
+};
+
+let bitonic = new Bitonic(arr);
+console.log('INITIAL BITONIC', bitonic);
+console.log('HAS 1?', bitonic.contains(1));
+console.log('HAS 10?', bitonic.contains(10));
+console.log('HAS 7?', bitonic.contains(7));
+let abs = new AlternateBinarySearch();
+console.log(abs.findMax([1, 35, 80, 70]));
