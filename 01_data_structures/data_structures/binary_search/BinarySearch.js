b2a8eb7f77dbbc12fb3b8378294b13aacb8b20b8
tomatagolo <tom@agolo.com>
2016-04-03 14:51:06 -0400

    add benchmarks for binary search

--- 01_data_structures/data_structures/binary_search/BinarySearch.js
+++ 01_data_structures/data_structures/binary_search/BinarySearch.js
@@ -20,6 +20,25 @@ class BinarySearch{
 
 
 let search = new BinarySearch();
-console.log(search.rank(1, [2, 3, 4]));
-console.log(search.rank(5, [2, 3, 4, 5]));
-console.log(search.rank(1, [1, 2, 3]));
+
+function generateArray(N){
+  return new Array(N).fill(0).map((v,i) => Math.floor(Math.random()*N)).sort();
+};
+
+function testSearch(N){
+  let d1 = new Date().valueOf();
+  let arr = generateArray(N);
+  console.log('ARR', arr);
+  let query = Math.floor(Math.random()*N);
+  let res = search.rank(query, arr);
+  let d2 = new Date().valueOf();
+  console.log(`Query for ${query} returned ${res} for array size ${arr.length} in ${d2 - d1} milliseconds`);
+  console.log(`Expected answer: ${query}, Actual: ${arr[res]}`);
+  d2 = null, d1 = null, query = null, res = null, arr = null;
+};
+
+let size = 10;
+while (size < 11){
+  testSearch(size);
+  size *= 10;
+}
