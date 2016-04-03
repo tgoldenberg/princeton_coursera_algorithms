f580d32e25f2153890a9d81494f00a6dc2a00bf2
lowellmower <lowell.mower@gmail.com>
2016-04-03 16:02:13 -0400

    add queue.rb

---
+++ 01_data_structures/data_structures/queue/queue.rb
@@ -0,0 +1,41 @@
+class Node
+
+	attr_accessor :item, :next
+
+	def initialize(args = {})
+		@item = args[:i] || nil
+		@next = args[:n] || nil
+	end
+
+end
+
+class Queue
+
+	def initialize
+		@head = nil
+		@tail = nil
+	end
+
+	def empty?
+		@head.nil?
+	end
+
+	def push(i)
+		if @tail
+			@tail.next = Node.new(i:i)
+			@tail = @tail.next
+		end
+		@head = @tail = Node.new(i:i)
+	end
+
+	def pop
+		unless @head
+			raise RuntimeError.new('No items in Queue')
+		end
+		result = @head.item
+		@head = @head.next
+		@tail = nil unless @head
+		result
+	end
+
+end
