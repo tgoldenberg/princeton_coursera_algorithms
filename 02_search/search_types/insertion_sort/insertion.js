257b7d39dca49eff97835bb0c1d739d7993c9219
tomatagolo <tom@agolo.com>
2016-04-04 20:18:27 -0400

    add insertion sort

---
+++ 02_search/search_types/insertion_sort/insertion.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var fs = require('fs');
+
+class Insertion{
+  constructor() { }
+  less(i, j){
+    return i < j;
+  }
+  exch(arr, i, j){
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  sort(arr){
+    for (let i=1; i<arr.length; i++)
+    { // insert a[i] among all a[i-1]...
+      for (let j=i; j>0 && this.less(arr[j], arr[j-1]); j--)
+      { this.exch(arr, j, j-1); }
+    }
+    return arr;
+  }
+};
+
+let insertion = new Insertion();
+
+function isSorted(arr){
+  for (let i=1; i<arr.length; i++){
+    if (arr[i] < arr[i-1]) { return false; }
+  }
+  return true;
+};
+
+let filename = process.argv[2];
+
+function runSort(N){
+  let d1 = new Date().valueOf();
+  let arr = new Array(N).fill(0).map(() => Math.floor(Math.random()*N));
+  let sorted = insertion.sort(arr);
+  let d2 = new Date().valueOf();
+  let result = isSorted(sorted);
+  let msg = `For ${N} elements returned ${result} in ${d2 - d1} milliseconds\n`;
+  fs.appendFile(filename, msg);
+  console.log(msg);
+};
+
+fs.writeFile(filename, 'RESULTS FOR INSERTION SORT');
+let size = 10;
+while (size < 10000000){
+  runSort(size);
+  size *= 10;
+}
--- 02_search/search_types/selection_sort/SelectionSort.js
+++ 02_search/search_types/selection_sort/SelectionSort.js
@@ -1,18 +1,25 @@
 'use strict';
 
+var fs = require('fs');
+
 class SelectionSort{
   constructor() { }
+  exch(arr, i, j){
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
   sort(arr){
     for(let i=0; i<arr.length; i++){
       let smallest = arr[i];
+      let smallestIdx = i;
       for (let j=i+1; j<arr.length; j++){
         if (arr[j] < smallest){
           smallest = arr[j];
+          smallestIdx = j;
         }
       }
-      let temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
+      this.exch(arr, i , smallestIdx);
     }
     return arr;
   }
@@ -20,4 +27,29 @@ class SelectionSort{
 
 let selection = new SelectionSort();
 
+function runSort(N){
+  let arr = new Array(N).fill(0).map(() => Math.floor(Math.random()*N));
+  return selection.sort(arr);
+};
+
+function isSorted(arr){
+  for (let i=1; i<arr.length; i++){
+    if (arr[i] < arr[i-1]) { return false; } 
+  }
+  return true;
+};
+
+let size = 10;
+let filename = process.argv[2];
+fs.writeFile(filename, 'RESULTS OF SELECTION SORT');
+while (size < 1000000){
+  let d1 = new Date().valueOf();
+  let sorted = runSort(size);
+  let d2 = new Date().valueOf();
+  let sortFinished = isSorted(sorted);
+  let msg = `Sort of ${size} elements returned ${sortFinished} in ${d2 - d1} milliseconds\n`;
+  console.log(msg); 
+  fs.appendFile(filename, msg);
+  size *= 10;
+}
 
