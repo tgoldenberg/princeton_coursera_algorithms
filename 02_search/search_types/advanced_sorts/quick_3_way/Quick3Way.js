3111c0b46489cdafbd6dd7b350a8c65b34564609
tomatagolo <tom@agolo.com>
2016-04-06 09:40:43 -0400

    solve quick 3 way sort

---
+++ 02_search/search_types/advanced_sorts/quick_3_way/Quick3Way.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var fs = require('fs');
+
+class Quick3Way{
+  constructor(arr) { 
+    this.arr = this.shuffle(arr);
+    console.log('SHUFFLED ARRAY', this.arr);
+  }
+  shuffle(arr){
+    let idx, temp;
+    for (let i=0; i<arr.length; i++){
+      idx = Math.floor(Math.random()*arr.length);
+      temp = arr[i];
+      arr[i] = arr[idx];
+      arr[idx] = temp;
+    }
+    return arr;
+  }
+  exch(arr, i, j){
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  sort(){
+    this.quicksort(this.arr, 0, this.arr.length-1);
+    return this.arr; 
+  }
+  quicksort(arr, lo, hi){
+    if (hi <= lo) { return; }
+    let lt = lo, i = lo+1, gt = hi, v = arr[lo];
+    while (i <= gt) {
+      if (arr[i] < v)      { this.exch(arr, lt++, i++); }
+      else if (arr[i] > v) { this.exch(arr, i, gt--); }
+      else                 { i++; }
+    }
+    this.quicksort(arr, lo, lt-1);
+    this.quicksort(arr, gt+1, hi);
+  }
+};
+
+function isSorted(arr){
+  for(let i=1; i<arr.length; i++){
+    if (arr[i] < arr[i-1]) { return false; }
+  }
+  return true;
+};
+
+function runSort(N){
+  let arr = new Array(N).fill(0).map(() => Math.floor(Math.random()*N));
+  console.log('ORIGINAL ARRAY', arr);
+  let d1 = new Date().valueOf();
+  let quick = new Quick3Way(arr);
+  let sorted = quick.sort();
+  let result = isSorted(sorted);
+  console.log('SORTED ARRAY', sorted, result);
+  let d2 = new Date().valueOf();
+  let msg = `Sort return ${result} for size ${N} in ${d2 - d1} milliseconds\n`;
+  console.log(msg);
+};
+
+let size = 10; 
+while (size < 11){
+  runSort(size);
+  size *= 10;
+}
--- 02_search/search_types/advanced_sorts/quicksort/QuickSort.js
+++ 02_search/search_types/advanced_sorts/quicksort/QuickSort.js
@@ -4,13 +4,15 @@ var fs = require('fs');
 
 class Quick{
   constructor(arr) {
+    console.log('ORIGINAL ARRAY', arr);
     this.arr = this.shuffle(arr);
+    console.log('SHUFFLED ARRAY', this.arr);
   }
   shuffle(arr){
     let temp, idx;
     for (let i=0; i<arr.length; i++){
       temp = arr[i];
-      idx = Math.floor(Math.random()*arr.length-1);
+      idx = Math.floor(Math.random()*arr.length);
       arr[i] = arr[idx];
       arr[idx] = temp;
     }
@@ -21,8 +23,10 @@ class Quick{
     return this.arr;
   }
   quicksort(arr, lo, hi){
+    console.log('LO HI', lo, hi);
     if (hi <= lo) { return; }
     let j = this.partition(arr, lo, hi);
+    console.log('J', j);
     this.quicksort(arr, lo, j-1);
     this.quicksort(arr, j+1, hi);
   }
@@ -46,7 +50,9 @@ class Quick{
       if (i >= j) { break; }
       this.exch(arr, i, j);
     }
+    console.log('PARTITION', arr);
     this.exch(arr, lo, j);
+    console.log('PARTITION 2', arr);
     return j; // with a[lo..j-1] <= a[j] <= a[j+1..hi]
   }
 };
@@ -63,6 +69,7 @@ function runSort(N){
   let d1 = new Date().valueOf();
   let sorted = quick.sort();
   let result = isSorted(sorted);
+  console.log('SORTED ARRAY', sorted, result);
   let d2 = new Date().valueOf();
   let msg = `Sort returned ${result} for size ${N} in ${d2 - d1} milliseconds\n`;
   console.log(msg);
@@ -70,7 +77,7 @@ function runSort(N){
 
 let size = 10;
 
-while (size < 100000000){
+while (size < 11){
   runSort(size);
   size *= 10;
 }
